91e3684cd55ab69cc2740516b3c9700b24f9d523
Hans Meyer <hamster.of.dev@gmail.com>
2016-03-25 12:09:20 +0100

    Add marshal interfaces for easier serialization

--- info/container.go
+++ info/container.go
@@ -1,7 +1,12 @@
 package info
 
+import "encoding"
+
 type Container interface {
 	Get(domPath string) interface{}
 	Set(domPath string, value interface{} error
 	String(template string) string
+
+	encoding.BinaryMarshaler
+	encoding.BinaryUnmarshaler
 }
