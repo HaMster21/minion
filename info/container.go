a0ef1838c617622e2974ce0769333206fc532579
Hans Meyer <hamster.of.dev@gmail.com>
2016-03-25 12:45:53 +0100

    Getting closer to being a container

--- info/container.go
+++ info/container.go
@@ -5,7 +5,7 @@ import "encoding"
 type Container interface {
 	Get(domPath string) interface{}
 	Set(domPath string, value interface{} error
-	String(template string) string
+	TplString(template string) string
 
 	encoding.BinaryMarshaler
 	encoding.BinaryUnmarshaler
--- task.go
+++ task.go
@@ -1,5 +1,24 @@
 package minion
 
+import (
+	"bytes"
+	"text/template"
+)
+
+var (
+	currentPattern = "{{.description}}"
+	currentTemplate *template.Template = nil
+)
+
+func init() {
+	// get user-defined pattern from the config
+	tpl, err := template.New("task").Parse(currentPattern)
+	if err != nil {
+		panic (err)
+	}
+	currentTemplate = tpl
+}
+
 type Task map[string]interface{}
 
 func NewTask(description string) Task {
@@ -15,4 +34,34 @@ func (t *Task) Get(at string) interface{} {
 func (t *Task) Set(at string, to interface{}) error {
 	map[string]interface{}(*t)[at] = to
 	return nil
+}
+
+func (t *Task) TplString(templ string) string {
+	var tpl *template.Template
+	var err error
+	var buf bytes.Buffer
+
+	tpl, err = template.New("task").Parse(templ + " {{ .description }}")
+	if err != nil {
+		tpl, err = template.New("task").Parse(currentPattern)
+		if err != nil {
+			panic (err)
+		}
+	}
+
+	err = tpl.Execute(&buf, map[string]interface{}(*t))
+	if err != nil {
+		panic(err)
+	}
+	return buf.String()
+}
+
+func (t *Task) String() string {
+	var buf bytes.Buffer
+
+	if err := currentTemplate.Execute(&buf, map[string]interface{}(*t)); err != nil {
+		panic (err)
+	}
+
+	return buf.String()
 }
